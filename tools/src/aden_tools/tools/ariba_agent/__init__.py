661b04c1377aa792a085d62a67e6b8cfbf213fbb
JYOTHIR RAGHAVALU BHOGI <113814770+jyothir-369@users.noreply.github.com>
2026-04-18 23:40:31 +0530

    Update __init__.py

--- tools/src/aden_tools/tools/ariba_agent/__init__.py
+++ tools/src/aden_tools/tools/ariba_agent/__init__.py
@@ -2,7 +2,5 @@
 # hive/tools/src/aden_tools/tools/ariba_agent/__init__.py
 
 from .tool import register_tools as register_ariba_tools
-from .ariba_agent import register_ariba_tools
-
 
 __all__ = ["register_ariba_tools"]
